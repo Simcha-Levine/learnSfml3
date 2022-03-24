345c6ceb5fa52869ad6d64b0f638470f541ca1fe
simcha <simcha.levine.10101@gmaile.com>
2022-03-24 15:40:09 +0200

    now it meson

--- compile.sh
+++
@@ -1,9 +0,0 @@
-if g++ -o hello main.cpp Ground.cpp Body.cpp Axis.cpp Poly.cpp ContactListener.cpp \
-    -lbox2d \
-    -lsfml-graphics -lsfml-window -lsfml-system \
-    -pedantic-errors -Wall -Wextra -Weffc++; then
-    echo "compiled successfully"
-    ./hello
-fi
-#-Weffc++
-# -Wsign-conversion
